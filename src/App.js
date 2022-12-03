a848b4df9c1f4eead951aadeeef13b43183cf9c8
BuildTools <unconfigured@null.spigotmc.org>
2022-12-04 00:45:33 +0700

    add milestone 1

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
 import Axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+
+  const bUrl = `${process.env.REACT_APP_BASEURL}`;
+  const bAPIKEY = `${process.env.REACT_APP_APIKEY}`;
+  const urlImage = `${process.env.REACT_APP_BASEIMGURL}`;
 
   const getData = () => {
     Axios({
       method: 'get',
-      url: `${process.env.REACT_APP_BASEURL}movie/popular?api_key=${process.env.REACT_APP_APIKEY}&language=en-US&page=1`,
+      url: `${bUrl}movie/popular?api_key=${bAPIKEY}&language=en-US&page=1`,
     })
       .then(function (response) {
         setFilem(response.data.results);
       });
   }
-  const urlImage = `${process.env.REACT_APP_BASEIMGURL}`
+
   const [filem, setFilem] = useState([]);
 
   useEffect(() => {
@@ -25,7 +30,7 @@ function App() {
 
   return (
     <>
-      <Navbar className='sticky-top' bg="dark" variant="dark">
+      <Navbar sticky='top' bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home" className='cssTitle'>Layar Tancap DB</Navbar.Brand>
           <Navbar.Toggle />
@@ -38,22 +43,24 @@ function App() {
       </Navbar>
 
       <div className='container mt-5'>
+        <div>
+          <h1>Popular</h1>
+        </div>
         <div className='c1'>
           {filem.map((item, index) => {
             return (
-              <Card style={{ width: '18rem' }} key={index}>
-                <Card.Img variant='top' src='https://image.tmdb.org/t/p/w500/{item.backdrop_path}' />
+              <Card key={index}>
+                <Card.Img variant='top' src={`${urlImage}${item.poster_path}`} />
                 <Card.Body>
-                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Title>{item.title} | {item.vote_average}/10</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">Movie ID: {item.id}</Card.Subtitle>
+                  <Card.Subtitle className="mb-2 text-muted">Popularity: {item.popularity}</Card.Subtitle>
                   <Card.Text>
                     {item.overview}
                   </Card.Text>
-
-                  <Card.Link href="#">Popularity: {item.popularity}</Card.Link>
-                  <Card.Link href="#">{item.vote_average}</Card.Link>
                 </Card.Body>
               </Card>
+
             );
           })}
         </div>
